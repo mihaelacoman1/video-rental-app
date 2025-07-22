42eb634b2e5eeb85484dc1728e0affc208c38f99
miha1110 <144226254+miha1110@users.noreply.github.com>
2025-07-22 17:50:25 +0300

    Update FormClienti.Designer.cs

--- VideoRentalApp/FormClienti.Designer.cs
+++ VideoRentalApp/FormClienti.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class FormClient
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,11 +14,7 @@
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+            
         private void InitializeComponent()
         {
             this.textBoxNume = new System.Windows.Forms.TextBox();
@@ -125,9 +114,7 @@
             this.Text = "FormClient";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
         #endregion
 
         private System.Windows.Forms.TextBox textBoxNume;
