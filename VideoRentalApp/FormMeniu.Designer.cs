383ec02358d6d10e3bb25f3520aef3f2287e2531
mihaelacoman1 <144226254+mihaelacoman1@users.noreply.github.com>
2025-07-29 15:42:58 +0300

    Update FormMeniu.Designer.cs

--- VideoRentalApp/FormMeniu.Designer.cs
+++ VideoRentalApp/FormMeniu.Designer.cs
@@ -2,14 +2,8 @@
 {
     partial class FormMeniu
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
@@ -21,10 +15,6 @@
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
